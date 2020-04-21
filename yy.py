d52175fe35df22a23f2c39a2b2ecb3f9135a63c2
hellojinji <1721821109@qq.com>
2020-04-21 11:12:46 +0800

    the new branch

--- yy.py
+++ yy.py
@@ -26,10 +26,7 @@ class NewVisitorTest(unittest.TestCase):  #1
 
         # She is invited to enter a to-do item straight away
         inputbox = self.browser.find_element_by_id('id_new_item')  #1
-        self.assertEqual(
-            inputbox.get_attribute('placeholder'),
-            'Enter a to-do item'
-        )
+      
 
         # She types "Buy peacock feathers" into a text box (Edith's hobby
         # is tying fly-fishing lures)
@@ -42,10 +39,10 @@ class NewVisitorTest(unittest.TestCase):  #1
 
         table = self.browser.find_element_by_id('id_list_table') #1
         rows = table.find_elements_by_tag_name('tr') 
-        self.assertTrue(
-        any(row.text == '1: Buy peacock feathers' for row in rows),
-        "New to-do item did not appear in table"
-    )
+        self.assertIn('1:Buy peacock feathers',[row.text for row in rows])
+        self.assertIn('2:Use peacock feathers to make a fly',
+        [row.text for row in rows])
+        
 
         # There is still a text box inviting her to add another item. She
         # enters "Use peacock feathers to make a fly" (Edith is very
