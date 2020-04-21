f6cb49e90dfcdec0ab5a16b1d07cee73a33853fb
hellojinji <1721821109@qq.com>
2020-04-21 11:55:05 +0800

    the new branch

--- yy.py
+++ yy.py
@@ -14,8 +14,16 @@ class NewVisitorTest(unittest.TestCase):  #1
     def tearDown(self):  #3
         self.browser.quit()
 
+		
+    def check_for_row_in_list_table(self, row_text):
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn(row_text, [row.text for row in rows])
+		
     def test_can_start_a_list_and_retrieve_it_later(self):
-        # Edith has heard about a cool new online to-do app. She goes
+	
+
+       # Edith has heard about a cool new online to-do app. She goes
         # to check out its homepage
         self.browser.get('http://localhost:8000')
 
@@ -33,15 +41,22 @@ class NewVisitorTest(unittest.TestCase):  #1
         inputbox.send_keys('Buy peacock feathers')  #2
 
         # When she hits enter, the page updates, and now the page lists
-        # "1: Buy peacock feathers" as an item in a to-do list table
-        inputbox.send_keys(Keys.ENTER)  #3
-        time.sleep(1)  #4
+       # "1: Buy peacock feathers" as an item in a to-do list table
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
+        self.check_for_row_in_list_table('1: Buy peacock feathers')
 
-        table = self.browser.find_element_by_id('id_list_table') #1
-        rows = table.find_elements_by_tag_name('tr') 
-        self.assertIn('1:Buy peacock feathers',[row.text for row in rows])
-        self.assertIn('2:Use peacock feathers to make a fly',
-        [row.text for row in rows])
+    # There is still a text box inviting her to add another item. She
+    # enters "Use peacock feathers to make a fly" (Edith is very
+    # methodical)
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('Use peacock feathers to make a fly')
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
+
+    # The page updates again, and now shows both items on her list
+        self.check_for_row_in_list_table('1: Buy peacock feathers')
+        self.check_for_row_in_list_table('2: Use peacock feathers to make a fly')
         
 
         # There is still a text box inviting her to add another item. She
